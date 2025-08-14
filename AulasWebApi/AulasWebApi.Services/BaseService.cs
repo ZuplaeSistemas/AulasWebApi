753beea3729bd7e5b2d5f57d2dada3d7d79a645a
Maykon Granemann <maykondgranemann@gmail.com>
2025-08-14 20:59:59 -0300

    feat: criado o projeto para as services

---
+++ AulasWebApi/AulasWebApi.Services/BaseService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulasWebApi.Services
+{
+    public class BaseService
+    {
+        public void Create()
+        {
+
+        }
+        public void Read()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+        public void ReadById()
+        {
+
+        }
+    }
+}
