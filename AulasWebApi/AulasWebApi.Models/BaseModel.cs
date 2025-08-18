fb1dff97d884cc39657c49cac7a1e2e38261a626
Maykon Granemann <maykondgranemann@gmail.com>
2025-08-18 20:54:52 -0300

    feat(models): add class person and prop creatat in basemodel

--- AulasWebApi/AulasWebApi.Models/BaseModel.cs
+++ AulasWebApi/AulasWebApi.Models/BaseModel.cs
@@ -9,10 +9,11 @@ namespace AulasWebApi.Models
     public class BaseModel
     {
         public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         public override string ToString()
         {
-            return $"{this.Id}";
+            return $"{this.Id} - {this.CreatedAt}";
         }
     }
 }
